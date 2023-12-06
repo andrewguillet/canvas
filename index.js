fa2ba33aeffa05ccfb4f3dca3c4be1efe789eb77
andrewguillet <149383802+andrewguillet@users.noreply.github.com>
2023-12-06 16:43:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const canvas = document.getElementById("art");
+const ctx = canvas.getContext("2d");
+
+function getMousePos(e) {
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: e.clientX - rect.left,
+    y: e.clientY - rect.top,
+  };
+  console.log(rect);
+}
+
+function mouseMove(e) {
+  const mousePos = getMousePos(e);
+  ctx.lineTo(mousePos.x, mousePos.y);
+  ctx.stroke();
+  ctx.strokeStyle = "salmon";
+  ctx.lineWidth = 8;
+}
+
+canvas.addEventListener("mousedown", (e) => {
+  e.preventDefault();
+  const mousePos = getMousePos(e);
+  ctx.beginPath();
+  ctx.moveTo(mousePos.x, mousePos.y);
+
+  canvas.addEventListener("mousemove", mouseMove);
+  canvas.addEventListener("mouseup", () => {
+    canvas.removeEventListener("mousemove", mouseMove);
+  });
+});
+
+reset.addEventListener("click", () => {
+  //   ctx.clearRect(0, 0, 800, 400); pareil que celui bas
+  ctx.clearRect(0, 0, canvas.width, canvas.heigth);
+});
